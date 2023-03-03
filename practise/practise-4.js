bf5ddd378b5f226e536e40a0d996d7bd66ae17b1
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-03-03 20:34:23 +0600

    practise problem

---
+++ practise/practise-4.js
@@ -0,0 +1,15 @@
+
+
+const func = num => {
+    return num * 7;
+}
+
+const arr = [1, 2, 3, 4];
+const value = arr.map(func);
+console.log(value);
+
+//*direct process
+const numbers = [7, 14, 21, 28];
+const val = numbers.map(n => n / 7)
+
+console.log(val);
---
+++ practise/practise-5.js
@@ -0,0 +1,12 @@
+const fish = {
+    name: 'king Hilsha',
+    address: 'chandpur',
+    color: 'silver',
+    phone: '01627869019',
+    price: 4000,
+
+}
+
+const { name, color } = fish;
+console.log(name);
+console.log(color);
